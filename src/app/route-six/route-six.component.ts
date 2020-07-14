8743349557b9fc94c07a6f0e4b99f7ac01e3cb64
ritugupta64 <31064674+ritugupta64@users.noreply.github.com>
2020-07-14 12:25:53 +0530

    fix

--- src/app/route-six/route-six.component.ts
+++ src/app/route-six/route-six.component.ts
@@ -1,28 +1,26 @@
-import { Component, OnInit, ElementRef, Renderer2, ViewChild } from '@angular/core';
-
+import {
+  Component,
+  HostListener,
+} from '@angular/core';
 
 @Component({
   selector: 'app-route-six',
   templateUrl: './route-six.component.html',
-  styleUrls: ['./route-six.component.css']
+  styleUrls: ['./route-six.component.css'],
 })
 export class RouteSixComponent {
+  public items: number[] = [1];
 
-  @ViewChild('abcd')
-
-  private abcd: ElementRef;	
-  constructor(private renderer: Renderer2) {
-  }
-  onClick() {
-    const div = this.renderer.createElement('div');
-    div.classList.add('dynamic-btn');
-    const btn = this.renderer.createElement('button');
-    btn.classList.add('btn', 'btn-primary');
-    const btnText = this.renderer.createText('button');
-    // const text = this.renderer.createText('Click here to add li');
-    this.renderer.appendChild(btn, btnText);
-    this.renderer.appendChild(div, btn)
-    this.renderer.appendChild(this.abcd.nativeElement, div);
+  @HostListener('window:scroll')
+  scrollItems(): boolean {
+    if (this.items.length !== 80) {
+      this.items.push(this.items.length + 1);
+    } else {
+      return false;
+    }
   }
 
+  getClickedItems(i) {
+    alert(`div ${i} clicked`);
+  }
 }
